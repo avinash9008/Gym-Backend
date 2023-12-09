6de8b07ac6a9e5e5d2faf1e5c05b8c6f2782ce52
nikita <nikitamane2000.com>
2023-12-09 19:09:22 +0530

    ff

--- Gym-parent/Gym-data/src/main/java/com/gym/models/Customer.java
+++ Gym-parent/Gym-data/src/main/java/com/gym/models/Customer.java
@@ -1,7 +1,5 @@
 package com.gym.models;
 
-import java.util.Date;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,29 +13,28 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	@Column
 	private String firstname;
 	private String lastname;
 	@Column
 	private String phone;
 	private String session;
-	private Date joiningDate;
-	private String amount;
-	
+	private String email;
 	
 	public Customer() {}
 	
-	public Customer(long id, String firstname, String lastname, String phone, String session, Date joiningDate,
-			String amount) {
+	
+
+	public Customer(long id, String firstname, String lastname, String phone, String session, String email) {
 		super();
 		this.id = id;
 		this.firstname = firstname;
 		this.lastname = lastname;
 		this.phone = phone;
 		this.session = session;
-		this.joiningDate = joiningDate;
-		this.amount = amount;
+		this.email = email;
 	}
+
+
 
 	public long getId() {
 		return id;
@@ -47,51 +44,64 @@ public class Customer {
 		this.id = id;
 	}
 
+
+
 	public String getFirstname() {
 		return firstname;
 	}
 
+
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
-   public String getLastname() {
+
+
+
+	public String getLastname() {
 		return lastname;
 	}
+
+
 
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
 
+
+
 	public String getPhone() {
 		return phone;
 	}
+
+
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+
+
 	public String getSession() {
 		return session;
 	}
+
+
 
 	public void setSession(String session) {
 		this.session = session;
 	}
 
-	public Date getJoiningDate() {
-		return joiningDate;
+
+
+	public String getEmail() {
+		return email;
 	}
 
-	public void setJoiningDate(Date joiningDate) {
-		this.joiningDate = joiningDate;
-	}
 
-	public String getAmount() {
-		return amount;
-	}
 
-	public void setAmount(String amount) {
-		this.amount = amount;
+	public void setEmail(String email) {
+		this.email = email;
 	}
   
 	
